f8e3a2967da09be7a97b81e5a1f6c582b8de5093
Raul Rodriguez Proenza <rproenza86@gmail.com>
2018-01-16 11:51:50 -0500

    feat: basic test

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
-}
+    sum (num1 = 1, num2 = 2) {
+      return num1 + num2;
+    }
+
+    render() {
+        return ( <div className="app_shell" > React simple starter </div>);
+        }
+    }
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,22 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+describe('App', () => { // use 'describe' to group similar tests
+    let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+    beforeEach(() => {
+        component = renderComponent(App);
+    });
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
+    it('renders something', () => { // use 'it' to test a single attribute of a target
+        expect(component).to.exist; // use 'expect' to make an 'assertion' about a target
+    });
+
+    it('correct class name', () => {
+        expect(component).to.have.class('app_shell');
+    });
+
+    it('show the correct text', () => {
+        expect(component).to.contain('React simple starter');
+    });
 });
