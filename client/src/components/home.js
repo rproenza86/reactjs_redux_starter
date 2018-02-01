9adb4f3e08a7464cfdd40bfca2b674d9b08c0787
Raul Rodriguez Proenza <rproenza86@gmail.com>
2018-02-01 17:20:26 -0500

    feat: added home page

---
+++ client/src/components/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+    render() {
+        return ( 
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">ReactJs + Redux Lab</h1>
+                    <p class="lead">Welcome to my ReactJs + Redux laboratory.</p>
+                </div>
+            </div>
+            );
+        }
+    }
