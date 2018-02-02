dfd12d3bb8aab079621c6c108f523a34eea8024e
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2018-02-01 23:47:32 -0500

    feat(sw): added main app bundle file to the cache, intercepting fetch events of get protocols to return first from cache then from network

--- client/sw.js
+++ client/sw.js
@@ -5,6 +5,7 @@ const cacheWhitelist = [CACHE_POSTS_IMAGES, CACHE_STATIC_ASSETS];
 
 const urlsToCache = [
     '/',
+    '/bundle.js',
     '/style/style.css',
     'https://cdn.rawgit.com/twbs/bootstrap/48938155eb24b4ccdde09426066869504c6dab3c/dist/css/bootstrap.min.css',
     'https://use.fontawesome.com/releases/v5.0.6/js/all.js'
@@ -39,4 +40,38 @@ self.addEventListener('message', event => {
     // Perform install steps without to wait next reload of app visit
     if (event.data.action === 'skipWaiting')
         self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+    // Let the browser do its default thing
+    // for non-GET requests.
+    if (event.request.method != 'GET') return;
+
+    const requestUrl = new URL(event.request.url);
+
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname === '/') {
+            event.respondWith(caches.match('/'));
+            return;
+        }
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request).then(response => {
+                if (response.status === 404) {
+                    return fetch('/imgs/dr-evil.gif');
+                }
+                return response;
+            }).catch(err => {
+                console.error(err);
+                return new Response("Resource request failed");
+            });
+        })
+    );
 });
